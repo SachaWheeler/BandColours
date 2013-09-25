48d65a7e1846cd55e1d191540c2eefb55cd76d5d
Sacha Wheeler <sacha@sachawheeler.com>
2013-09-25 10:24:11 +0100

    1st working version

--- functions.php
+++ functions.php
@@ -13,24 +13,44 @@ class LastFm{
 	public function __construct(){
 		$this->key = "03337138fe5149f05088428490c33f0b";
 		$this->domain = "http://ws.audioscrobbler.com/2.0/?api_key=".$this->key;
-		$this->artistSearchUrl = $this->domain."&method=artist.search&limit=100"; 
-		$this->tagSearchUrl = $this->domain."&method=artist.getTags"; 
+		$this->artistSearchUrl = $this->domain."&method=artist.search"; 
+		$this->tagSearchUrl = $this->domain."&method=artist.gettoptags"; 
 		// http://ws.audioscrobbler.com/2.0/?method=artist.getTags&artist=Red%20Hot%20Chili%20Peppers&user=RJ&api_key=03337138fe5149f05088428490c33f0b
 	}
 
 	public function searchArtist($term){
+		$artistArray = array();
 		if($term == '')
 			return;
-		$artists = new SimpleXMLElement(
-			$this->fetch($this->artistSearchUrl."&artist=".urlencode($term)));
-		print_r($artists);
+		$page=0;
+		while(count($artistArray)<100){
+			$artists = new SimpleXMLElement(
+				$this->fetch($this->artistSearchUrl.
+					"&artist=".urlencode($term).
+					"&page={$page}"));
+			$page++;
+			foreach($artists->results->artistmatches->artist as $artist){
+				$artistArray[] = $artist->name;
+			}
+		}
+		return $artistArray;
 	}
 
 	public function getArtistTags($artist){
+		$tagArray = array();
 		if($artist == '')
 			return;
-		$tags = new SimpleXMLElement($this->fetch($this->tagSearchUrl));
-		print_r($tags);
+		$tags = new SimpleXMLElement(
+			$this->fetch($this->tagSearchUrl.
+				"&artist=".urlencode($artist).
+				"&limit=5"));
+		foreach($tags->toptags->tag as $tag){
+			if(count($tagArray) ==5)
+				break;
+			if($tag->count > 0)
+				$tagArray[] = $tag->name;
+		}
+		return $tagArray;
 	}
 	
 	protected function fetch($url){
--- run.php
+++ run.php
@@ -1,3 +1,24 @@
 <?php
 
+include_once("functions.php");
 
+$lastfm = new LastFm();
+
+$colours = array("red",
+				"orange",
+				"yellow",
+				"green",
+				"blue",
+				"indigo",
+				"violet",
+				"white",
+				"grey",
+				"black");
+
+foreach($colours as $colour){
+	foreach($lastfm->searchArtist($colour) as $artist){
+		echo $artist."\n";
+		print_r($lastfm->getArtistTags($artist));
+		sleep(1);
+	}
+}
