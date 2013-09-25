be4e1b985a2bdda099084d8317deef3a420d79d9
Sacha Wheeler <sacha@sachawheeler.com>
2013-09-25 09:22:32 +0100

    objects

--- functions.php
+++ functions.php
@@ -5,3 +5,17 @@
 //
 
 $lastfm_api_key = "03337138fe5149f05088428490c33f0b";
+
+class LastFm{
+	protected $lastfmKey;
+
+	public function __construct(){
+		$this->lastfmKey = "03337138fe5149f05088428490c33f0b";
+	}
+
+	public function search($term){
+	}
+
+	public function(){
+	}
+}
